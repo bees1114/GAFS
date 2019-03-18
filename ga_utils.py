388924dcba2e99e3cfd78c901fbc8b4b90c48e44
bees1114 <bees1114@naver.com>
2019-03-19 00:06:52 +0900

    make test available with titanic dataset. + smallest refac in ga_utils.py

--- ga_utils.py
+++ ga_utils.py
@@ -1,6 +1,7 @@
 from deap import base, creator, tools
 from sklearn.tree import DecisionTreeClassifier
 
+
 def evaluate(individual, evaluator):
     evaluation = DecisionTreeClassifier()
     evaluation.fit(individual )
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from sklearn import datasets
 from base import GeneticFeatureSelection
+
 
 import pandas as pd
 import sys
@@ -7,10 +7,25 @@ import sys
 FILE_PATH = 'D:/git/GAFS/dataset/titanic/'
 
 data = pd.read_csv(FILE_PATH + 'train.csv')
-#print(data.isnull().sum())
-#
+
 y = data[['Survived']]
 X = data.drop(['Survived'], axis=1)
+
+# One hot Encoding
+Pclass = pd.get_dummies(X['Pclass'], 'Pclass')
+Sex = pd.get_dummies(X['Sex'], columns=['M', 'W'])
+SibSp = pd.get_dummies(X['SibSp'], 'SibSp')
+Parch = pd.get_dummies(X['Parch'], 'Parch')
+Embarked = pd.get_dummies(X['Embarked'], 'Embarked')
+Cabin = pd.DataFrame([1 if pd.notnull(i) else 0 for i in X['Cabin']], columns=['new_Cabin'])
+
+# Make features
+X = pd.concat([X, Pclass, Sex, SibSp, Parch, Embarked, Cabin], axis=1)
+X = X.drop(['PassengerId', 'Pclass', 'Sex', 'Name', 'SibSp', 'Parch', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+X = X.fillna(0)
+
+print(X.head())
 print(X.columns)
+
 gfs = GeneticFeatureSelection(generations=2, population_size=5)
 gfs.fit(X, y)
